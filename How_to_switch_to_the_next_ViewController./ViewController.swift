8e3769697be9485ffda316970971b7281bf233ff
kimjunsoo <rlawnstn01023@naver.com>
2021-10-07 15:32:59 +0900

    Initial Commit

---
+++ How_to_switch_to_the_next_ViewController./ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  How_to_switch_to_the_next_ViewController.
+//
+//  Created by 준수김 on 2021/10/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
